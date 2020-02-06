66ad5e1d7e3b9186100fd037e98b7b6c5e673ad1
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-02-06 11:23:56 -0800

    Added Note and Folder component scaffolding

---
+++ src/components/Sidebar/Folder/Folder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Folder.css';
+
+export default class Folder extends Component {
+
+	render() {
+	  return (
+	    <div className="Folder">
+	    	<p>
+	    		Folder
+	    	</p>
+	    </div>
+	  );		
+	}
+
+}
