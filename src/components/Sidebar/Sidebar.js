a1830f79108dccd5e023a18650904a38c43ed5ea
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-02-06 11:17:21 -0800

    First commit. Scaffolding in place

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Sidebar.css';
+
+export default class Sidebar extends Component {
+
+	render() {
+	  return (
+	    <div className="Sidebar">
+	    	<p>
+	    		Sidebar
+	    	</p>
+	    </div>
+	  );		
+	}
+
+}
---
+++ src/components/Sidebar/Sidebar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import Sidebar from './Sidebar';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+  	<BrowserRouter>
+  		<Sidebar />
+  	</BrowserRouter>,
+  	div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
