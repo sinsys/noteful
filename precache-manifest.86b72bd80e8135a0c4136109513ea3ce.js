9130319c4f9a82ecb76e78df7aaf37ae4cc563b2
Nicholas Hazel <nicholas.hazel@gmail.com>
2020-02-07 12:52:44 -0800

    Updates

---
+++ precache-manifest.86b72bd80e8135a0c4136109513ea3ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74981ddd4e0a56968a51ae543c89fb7",
+    "url": "/noteful/index.html"
+  },
+  {
+    "revision": "e22ac4555d4695be1e50",
+    "url": "/noteful/static/css/main.1d0a1454.chunk.css"
+  },
+  {
+    "revision": "730b9d4054764d9bed00",
+    "url": "/noteful/static/js/2.c11e600b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/noteful/static/js/2.c11e600b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22ac4555d4695be1e50",
+    "url": "/noteful/static/js/main.4d5bbe35.chunk.js"
+  },
+  {
+    "revision": "dd78170b687413ff9cd1",
+    "url": "/noteful/static/js/runtime-main.75b1ecd5.js"
+  }
+]);
